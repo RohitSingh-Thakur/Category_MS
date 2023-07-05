f93e47c75b537fe644ad2e38393bd377dc74055f
RohitSingh <thakurrohitdsingh@gmail.com>
2023-07-05 19:40:28 +0530

    Adding Category Microservice Project 05-07-2023

---
+++ src/main/java/com/singh/base/constants/Global_ExceptionConstants.java
@@ -0,0 +1,11 @@
+package com.singh.base.constants;
+
+public class Global_ExceptionConstants {
+	
+	public static final String RECORD_ALREADY_FOUND_OF_CATEGORY_EXCEPTION = "Record Already Exist In Database, Please Add Another Category";
+	public static final String NO_RECORD_FOUND_FOR_GIVEN_CATEGORY_NAME_EXCEPTION = "No Record Found With Given Category Name, Please Try With Another Category Name";
+	public static final String NO_RECORD_FOUND_FOR_GIVEN_CATEGORY_ID_EXCEPTION = "No Record Found For Given Category ID";
+	public static final String NO_RECORDS_FOUND_CATEGORY_EXCEPTION = "No Records Found, Add Category To Display Them";
+	public static final String NO_CATEGORY_RECORD_FOUND_TO_DELETE_EXCEPTION = "No Records Found For Given Category ID To Delete";
+	public static final String EMPTY_CATEGORY_EXCEPTION = "No Category To Display";	
+}
